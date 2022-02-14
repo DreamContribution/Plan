884b943d6e8701a3a8123970edd9e3c67e8dfcf5
A-学徒 <405625139@qq.com>
2022-02-14 22:52:50 +0800

    Demo of Icon's color setting.

--- app/src/main/java/com/frank/plan/ui/views/Pages.kt
+++ app/src/main/java/com/frank/plan/ui/views/Pages.kt
@@ -1,14 +1,20 @@
 package com.frank.plan.ui.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 
@@ -31,4 +37,17 @@ fun HistoryList() {
         }
         BottomBar(modifier = Modifier.wrapContentHeight())
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TestOfSurface() {
+    Icon(
+        imageVector = Icons.Default.Add,
+        contentDescription = "Add ",
+        tint = Color.Blue,
+        modifier = Modifier.background(
+            Color.Red
+        )
+    )
 }
