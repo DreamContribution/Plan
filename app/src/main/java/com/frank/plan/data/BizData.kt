d58f1fbcb4f6064a5265c635fb6f06d0af0f5c5e
A-学徒 <405625139@qq.com>
2022-03-19 23:26:08 +0800

    Update the database and build the instance mode of BillDataBase

--- app/src/main/java/com/frank/plan/data/BizData.kt
+++ app/src/main/java/com/frank/plan/data/BizData.kt
@@ -1,9 +1,11 @@
 package com.frank.plan.data
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
 import androidx.room.*
 import com.frank.plan.MainActivity
+import kotlinx.coroutines.flow.Flow
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -11,7 +13,7 @@ import java.util.*
 
 @Entity
 data class Bill(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long? = null,
     val type: Int,
     val value: Double,
     val time: LocalDateTime = LocalDateTime.now()// android min 26
@@ -54,8 +56,8 @@ interface BillDao {
     @Insert
     fun addBill(vararg bills: Bill)
 
-    @Query("SELECT * FROM Bill")
-    fun getAllBill(): List<Bill>
+    @Query("SELECT * FROM Bill ORDER BY id DESC")
+    fun getAllBill(): Flow<List<Bill>>
 
 //    // TODO: SQL
 //    fun getBillsByMonth(): List<DayBill>
@@ -75,13 +77,30 @@ interface BillDao {
 @TypeConverters(Converters::class)
 abstract class BillDataBase : RoomDatabase() {
     abstract fun billDao(): BillDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: BillDataBase? = null
+
+        fun getDatabase(context: Context): BillDataBase {
+            return INSTANCE ?: synchronized(this) {
+                val instant = Room.databaseBuilder(
+                    context,
+                    BillDataBase::class.java,
+                    "bill"
+                ).build()
+                INSTANCE = instant
+                return instant
+            }
+        }
+    }
 }
 
 
 /**
  * test of data
  */
-fun dataBaseTest(context: Application) {
+suspend fun dataBaseTest(context: Application) {
     val db =
         Room.databaseBuilder(context, BillDataBase::class.java, "bill")
             .build()
@@ -91,7 +110,8 @@ fun dataBaseTest(context: Application) {
     val itemBill = Bill(value = 20.0, type = 1)
     Log.d(MainActivity.TAG, "onCreate: itemBill:$itemBill")
     billDao.addBill(Bill(value = 20.0, type = 1))
-
-    val allBill = billDao.getAllBill()[0]
+    val allBill = billDao.getAllBill().collect {
+        it[0]
+    }
     Log.d(MainActivity.TAG, "onCreate: allBill:$allBill")
 }
